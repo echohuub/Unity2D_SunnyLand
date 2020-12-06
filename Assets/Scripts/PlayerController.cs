5cab38094c65173d0e41ecc17b7316f694d81294
heqingbao <qingbao.ho@gmail.com>
2020-12-06 19:52:00 +0800

    跑动动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
     public float speed;
     public float jumpForce;
 
@@ -24,15 +25,20 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMove = Input.GetAxis("Horizontal");
         float faceDirection = Input.GetAxisRaw("Horizontal");
+
+        // 角色移动
         if (horizontalMove != 0)
         {
             rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(faceDirection));
         }
+        // 角色向左向右
         if (faceDirection != 0)
         {
             transform.localScale = new Vector3(faceDirection, 1, 1);
         }
 
+        // 角色跳跃
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
