c02273af1e570c65ec84b870730dc87ea818e088
heqingbao <qingbao.ho@gmail.com>
2020-12-06 18:08:43 +0800

    简单移动

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalMove;
+        horizontalMove = Input.GetAxis("Horizontal");
+        if (horizontalMove != 0)
+        {
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+        }
+    }
+}
